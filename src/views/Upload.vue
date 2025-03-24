1666da8fe9a4b919e7290f25a47a44a1679acca5
PoXel6 <iampoxel6@gmail.com>
2025-03-24 16:48:40 +0330

    feat(views): made upload page.

--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,15 +1,17 @@
 <template>
-  <div class="upload">
-    <h1>Upload your shit here page.</h1>
+  <div class="upload flex justify-center items-center bg-red-500 max-h-screen">
+    <main
+        class="text-center bg-gray-800 text-gray-100 border-red-300 border-3 rounded-md h-full w-full p-3">
+      <div class="min-h-1/4">
+        <h1 class="text-xl flex items-center justify-center">Upload your shit here</h1>
+      </div>
+      <div
+          class="border-3 border-red-200 rounded-lg bg-red-50 border-dashed flex flex-col justify-center items-center min-h-3/4">
+        <h1 class="text-gray-700 text-center">
+          Drag and Drop your File here
+        </h1>
+        <h2 class="text-sm text-gray-700 text-center">SVG PNG JPEG GIF</h2>
+      </div>
+    </main>
   </div>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .upload {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
