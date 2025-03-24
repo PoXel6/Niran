7adcb0f974a116a59eb80cc38e16eb9065199e7e
PoXel6 <iampoxel6@gmail.com>
2025-03-24 11:13:01 +0330

    feat(router): add new router for upload.

---
+++ src/views/Upload.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="upload">
+    <h1>Upload your shit here page.</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .upload {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
